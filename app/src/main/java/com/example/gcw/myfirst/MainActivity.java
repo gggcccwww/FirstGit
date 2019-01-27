4c641f99d1be7e023718bfdc9721fbff3c0d8d1a
gggcccwww <1562503433@qq.com>
2019-01-27 17:55:45 +0800

    second commit

--- app/src/main/java/com/example/gcw/myfirst/MainActivity.java
+++ app/src/main/java/com/example/gcw/myfirst/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.gcw.myfirst;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.hehe).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "嘎嘎", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
